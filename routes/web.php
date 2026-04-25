e2db1f3089b0de3ca006436ac21c85a0323e4fcb
syuen96 <syuenwoo0906@gmail.com>
2026-04-25 17:05:36 +0800

    Solve merge conflict web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use App\Models\Category;
-use App\Models\Expense;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
+use App\Models\Expense;     
 
 
 Route::get('/register', [AuthController::class, 'showRegister']);
@@ -44,3 +45,12 @@ Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->middlew
 Route::get('/profile',[ProfileController::class,'index'])->middleware('auth');
 Route::view('/profile/edit', 'profile.edit')->middleware('auth')->name('profile.edit');
 Route::put('/profile', [ProfileController::class, 'update'])->middleware('auth');
+
+Route::middleware(['auth', 'can:isAdmin'])->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/create', [UserController::class, 'create']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}/edit', [UserController::class, 'edit']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+});
