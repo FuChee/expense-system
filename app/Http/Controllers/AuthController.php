4b0b96b792b47469026d8fa18d57d9e16e4c803f
syuen96 <syuenwoo0906@gmail.com>
2026-04-25 19:38:25 +0800

    Revert "update logout"
    This reverts commit a999d12963bd05a7688a15bae9bbf8c62a8f65e0.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,10 +60,6 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout();
-        
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
         return redirect('/login');
     }
 }
