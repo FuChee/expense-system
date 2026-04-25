6aee7eb8f2e35f843c9a6daed67a4e0e9a9f36c2
syuen96 <syuenwoo0906@gmail.com>
2026-04-25 19:51:33 +0800

    Modify logout in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,9 +57,13 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
         Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
         return redirect('/login');
     }
 }
